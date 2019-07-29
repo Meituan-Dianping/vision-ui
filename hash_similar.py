d9bf13f0d3d9e6b98402cef9342a37c3c50a4534
靳海亮 <jinhailiang@meituan.com>
2019-07-29 15:12:03 +0800

    update image attention

--- hash_similar.py
+++ hash_similar.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2,numpy
 
 
 class HashSimilar(object):
@@ -44,6 +44,26 @@ class HashSimilar(object):
         return sum([ch1 != ch2 for ch1, ch2 in zip(s1, s2)])
 
     @staticmethod
+    def get_attention(img1, img2):
+        """
+        calculate images attention score list
+        :param img1: input image A
+        :param img2: input image B
+        :return: score list
+        """
+        img1_list = HashSimilar.get_image_list(img1)
+        img2_list = HashSimilar.get_image_list(img2)
+        l = min(len(img1_list), len(img2_list))
+        score_list = []
+        for i in range(0, l, 1):
+            start = i - 5 if i - 5 >= 0 else 0
+            img_stack = numpy.vstack(img2_list[start:i+5])
+            res = cv2.matchTemplate(img1_list[i], img_stack, cv2.TM_CCOEFF_NORMED)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            score_list.append(max_val)
+        return score_list
+
+    @staticmethod
     def get_similar(image1, image2):
         """
         calculate image content similar score
@@ -54,23 +74,17 @@ class HashSimilar(object):
             - 0.8 : image has increment content
             - 1.0 : imageA is similar to imageB
         """
-        precision = 16
-        img1_list = HashSimilar.get_image_list(HashSimilar.get_image(image1))
-        img2_list = HashSimilar.get_image_list(HashSimilar.get_image(image2))
+        img1 = HashSimilar.get_image(image1)
+        img2 = HashSimilar.get_image(image2)
+        std1 = numpy.std(img1)
+        std2 = numpy.std(img2)
+        img1_list = HashSimilar.get_image_list(img1)
+        img2_list = HashSimilar.get_image_list(img2)
         l = min(len(img1_list), len(img2_list))
-        score_list = []
-        for i in range(0, l, 2):
-            hash1 = HashSimilar.perception_hash(img1_list[i], precision)
-            score_hash = []
-            for j in range(i-5, i+5):
-                if 0 <= j < l:
-                    hash2 = HashSimilar.perception_hash(img2_list[j], precision)
-                    score = 1 - HashSimilar.hamming_dist(hash1, hash2) * 1.0 / (precision * precision)
-                    score_hash.append(score)
-            score_list.append(max(score_hash))
-
+        score_list = HashSimilar.get_attention(img1, img2)
         score_list.sort()
-        if score_list[int(len(score_list)*0.8)-1] < 0.8:
+        if score_list[int(len(score_list) * 0.8) - 1] < 0.8 or max(len(img1_list), len(img2_list)) - l > l \
+                or abs(std1 - std2) > 35:
             return 0.2
         if min(score_list) > 0.99:
             return 1.0
