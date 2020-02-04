3717b39d1a4eb5fe1caf2fbefb2660b876c9565c
jhl <jinhailiang@meituan.com>
2020-02-04 11:52:13 +0800

    server api with blue print

--- server.py
+++ server.py
@@ -1,52 +1,13 @@
 from flask import Flask
 from flask import jsonify
-from flask import request
-from utils.image_diff import ImageDiff
-from utils.image_merge import Stitcher
-from utils.image_similar import HashSimilar
-from utils.image_text import get_text_roi
 from flask_cors import CORS
+from api.vision_api import vision
 
 
 app = Flask(__name__)
 CORS(app)
 
-
-@app.route('/vision/diff', methods=["POST"])
-def vision_diff():
-    data = {
-        "code": 0,
-        "data": ImageDiff().get_image_score(request.json['image1'], request.json['image2'],
-                                            request.json['image_diff_name'])
-    }
-    return jsonify(data)
-
-
-@app.route('/vision/merge', methods=["POST"])
-def vision_merge():
-    data = {
-        "code": 0,
-        "data": Stitcher(request.json['image_list']).image_merge(request.json['name'])
-    }
-    return jsonify(data)
-
-
-@app.route('/vision/similar', methods=["POST"])
-def vision_similar():
-    data = {
-        "code": 0,
-        "data": HashSimilar().get_hash_similar(request.json['image1'], request.json['image2'])
-    }
-    return jsonify(data)
-
-
-@app.route('/vision/text', methods=["POST"])
-def vision_text():
-    data = {
-        "code": 0,
-        "data": get_text_roi(request.json['image'])
-    }
-    return jsonify(data)
+app.register_blueprint(vision, url_prefix='/vision')
 
 
 @app.errorhandler(Exception)
